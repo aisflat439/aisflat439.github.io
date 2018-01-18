7d3562d13910efdf439c3e2706ebfa0038a4eace
aisflat439 <fitzsimonsdevin@gmail.com>
2018-01-18 09:10:00 -0500

    add content for explainer.

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,17 @@ window.onload = function () {
 
     function hideAllShowCurrent(index) {
         var sectionElements = document.querySelectorAll('.js-explainer-area');
-        
+        var useExplanations = document.querySelectorAll('.rule-explanation__content');
+
         sectionElements.forEach(item => {
             item.classList.toggle('show');
         })
         sectionElements[index].className += ' show';
+
+        useExplanations.forEach(item => {
+            item.classList.toggle('visible');
+        });
+        useExplanations[index].className += ' visible'; 
     }
     
     var setShowAndHideListeners = function(buttons) {
