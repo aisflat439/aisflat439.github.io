f6e8707f5561ce6e48dcdaac2bd9abaae1b67d23
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2018-06-26 09:33:59 -0400

    added some mixins and refactored a bit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+// window.onload = function () {
+//     const modalToggle = element => {
+//         element.toggleClass('active')
+//     }
+
+//     const modals = document.querySelectorAll('[data-modal]')
+
+//     [...modals].forEach(modal => modal.addEventListener(modalToggle))
+// }
